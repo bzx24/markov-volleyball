0f4a8dc8b352bf58823bdb0c0385fc505c2145d4
bzx24 <brianxu16@gmail.com>
2021-08-17 18:49:11 -0500

    initial commit

---
+++ markov.R
@@ -0,0 +1,15 @@
+library(datavolley)
+
+#read in data
+files <- list.files(path = "./dvw_files", pattern = "\\.dvw$")
+x <- read_dv(paste0("./dvw_files/", files[1]))
+data <- x$plays
+# data$date <- as_date(x$meta$match$date)
+
+for(i in 2:length(files)) {
+  x <- read_dv(paste0("./dvw_files/", files[i]))
+  hold <- x$plays
+  # hold$date <- as_date(x$meta$match$date)
+  data <- rbind(data,hold)
+}
+remove(hold, x, i, files)
