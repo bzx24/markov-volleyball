6c0012e498e07a9c16774d732319d4739dcfbd44
bzx24 <brianxu16@gmail.com>
2021-08-18 17:33:19 -0500

    creating sequence

--- markov.R
+++ markov.R
@@ -1,17 +1,54 @@
 library(datavolley)
+library(dplyr)
 
 #read in data
 files <- list.files(path = "./dvw_files", pattern = "\\.dvw$")
 x <- read_dv(paste0("./dvw_files/", files[1]))
 data <- x$plays
-# data$date <- as_date(x$meta$match$date)
 
 for(i in 2:length(files)) {
   x <- read_dv(paste0("./dvw_files/", files[i]))
   hold <- x$plays
-  # hold$date <- as_date(x$meta$match$date)
   data <- rbind(data,hold)
 }
 remove(hold, x, i, files)
 
-#hello
+#tidy data
+markov_data <- data %>%
+  filter(!is.na(skill)) %>%
+  select(point_id, skill, skill_type, evaluation, start_zone, end_zone, end_subzone)
+
+#create sequence of states
+sequences <- character(0)
+for (row in 1:nrow(markov_data)) {
+  
+  #only include first possession
+  if (markov_data[row, "skill"] == "Dig" | markov_data[row, "skill"] == "Block" | markov_data[row, "skill"] == "Freeball" | markov_data[row, "skill"] == "Technical Timeout") next
+  
+  if (skill == "Set" & sequences[length(sequences)] != "Reception") next
+  
+  if (skill == "Attack" & sequences[length(sequences)] != "Set") next
+  
+  #append states to sequences
+  if (skill == "Reception") {
+    
+  }
+  
+  sequences <- append(sequences, state)
+  if (evaluation == "Winning attack") {
+    sequences <- append(sequences, "Rally Won")
+  } 
+  
+  else if (evaluation == "Error") {
+    sequences <- append(sequences, "Rally Lost")
+  } 
+  
+  else {
+    sequences <- append(sequences, "Rally Continuation")
+  }
+}
+
+#create markov chain object
+states <- c("Serve", "1A", "1B", "1C", "1D", "2A", "2B", "2C", "2D", "3A", "3B", "3C", "3D", "4A", "4B", "4C", "4D")
+
+
